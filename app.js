5f999ec146ccaa276588ea4ab6495c53dfb48804
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-05-27 11:22:35 +0530

    listen

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ const openLibraryRouter = require('./routes/openlibrary')
 const app = express()
 
 const port = process.env.PORT || 3000
-const server = app.listen(port, (err) => {
-    console.log(`API listening on ${port}!`)
-    if (err) throw err
-})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -27,5 +23,10 @@ app.use(cors());
 app.use('/', indexRouter)
 app.use('/libgen', libgenRouter)
 app.use('/openlibrary', openLibraryRouter)
+
+app.listen(port, (err) => {
+    console.log(`API listening on ${port}!`)
+    if (err) throw err
+})
 
 module.exports = app
